e1a1def62dce22f4abfee332740b7c22ed6fb5fa
nvmRegi <cabael.regine@gmail.com>
2022-04-26 12:56:55 +0200

    file

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Indirizzo
+    {
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private int ZIP;
+
+        public Indirizzo(string name, string surname, string street, string city, string province, int ZIP)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.ZIP = ZIP;
+        }
+    }
+}
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ListaIndirizzi;
+
+
 Console.WriteLine("Hello, World!");
+
+string path = "./addresses.csv";
+
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+
+StreamReader file = File.OpenText(path);
+
+while (!file.EndOfStream)
+{
+    string line = file.ReadLine();
+    string[] informazioni = line.Split(',');
+    
+    if(informazioni.Length == 6)
+    {
+        string nome = informazioni[0];
+        string cognome = informazioni[1];
+        string via = informazioni[2];
+        string citta = informazioni[3];
+        string provincia = informazioni[4];
+        int ZIP = int.Parse(informazioni[5]);
+
+        listaIndirizzi.Add(new Indirizzo(nome, cognome, via, citta, provincia, ZIP));
+    }
+    
+
+
+}
